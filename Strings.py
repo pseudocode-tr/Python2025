0cf749f368e60785b064987903f65fc7fd5a321c
Tris <engr.carillatristanpaul@gmail.com>
2025-07-04 15:41:13 +0800

    string functions

---
+++ Strings.py
@@ -0,0 +1,13 @@
+#Strings in Python
+import os
+os.system ('clear')
+
+greetings = 'I Created a code \n\"Hello World!"'
+
+print(greetings.split(" ")[1:2]) #split like list [ , , ,]
+'''functions .upper() to capitalize, 
+.lower(), .capitalize(), .title(),
+ .swapcase()switch upper to lower
+ len(greetings) to count the letters
+ add[1:desired range] to print designated string
+'''
